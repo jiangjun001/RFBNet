36f29d08e33984eba2aa6c8260431898ad5a6c87
jiangjun001 <49437389+jiangjun001@users.noreply.github.com>
2019-07-11 15:06:25 +0800

    Update DDCB_Net_vgg.py

--- models/DDCB_Net_vgg.py
+++ models/DDCB_Net_vgg.py
@@ -1,3 +1,20 @@
+class BasicConv(nn.Module):
+
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU(inplace=True) if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
 class BottleneckBlock(nn.Module):
     def __init__(self, in_planes, out_planes, dropRate=0.0, visual=1):
         super(BottleneckBlock, self).__init__()
@@ -20,28 +37,264 @@ class BottleneckBlock(nn.Module):
             out = F.dropout(out, p=self.droprate, inplace=False, training=self.training)
         return torch.cat([x, out], 1)
 
-class TransitionBlock(nn.Module):
-    def __init__(self, in_planes, out_planes, dropRate=0.0):
-        super(TransitionBlock, self).__init__()
+
+class DenseDDCB(nn.Module):
+    def __init__(self, nb_layers, in_planes, out_planes, growth_rate, block, dropRate=0.0):
+        super(DenseDDCB, self).__init__()
+        self.out_channels = out_planes
+        self.layer = self._make_layer(block, in_planes, growth_rate, nb_layers, dropRate)
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1,
+                               padding=0, bias=False)
+    def _make_layer(self, block, in_planes, growth_rate, nb_layers, dropRate):
+        layers = []
+        for i in range(nb_layers):
+            layers.append(block(in_planes+i*growth_rate, growth_rate, dropRate, visual=i+1))
+        return nn.Sequential(*layers)
+    def forward(self, x):
+        return self.relu(self.conv1(self.relu1(self.bn1(self.layer(x)))))
+    
+class DenseDDCB_a(nn.Module):
+    def __init__(self, nb_layers, in_planes, out_planes, growth_rate, block, dropRate=0.0):
+        super(DenseDDCB, self).__init__()
+        self.out_channels = out_planes // 2
+        self.layer = self._make_layer(block, in_planes, growth_rate, nb_layers, dropRate)
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.relu = nn.ReLU(inplace=True)
         self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1,
                                padding=0, bias=False)
         self.droprate = dropRate
-    def forward(self, x):
-        out = self.conv1(self.relu(self.bn1(x)))
-        if self.droprate > 0:
-            out = F.dropout(out, p=self.droprate, inplace=False, training=self.training)
-        return F.avg_pool2d(out, 2)
-
-class DenseDDCB(nn.Module):
-    def __init__(self, nb_layers, in_planes, growth_rate, block, dropRate=0.0):
-        super(DenseDDCB, self).__init__()
-        self.layer = self._make_layer(block, in_planes, growth_rate, nb_layers, dropRate)
     def _make_layer(self, block, in_planes, growth_rate, nb_layers, dropRate):
         layers = []
         for i in range(nb_layers):
-            layers.append(block(in_planes+i*growth_rate, growth_rate, dropRate, visual=i))
+            layers.append(block(in_planes+i*growth_rate, growth_rate, dropRate, visual=i+1))
         return nn.Sequential(*layers)
     def forward(self, x):
-        return self.layer(x)
+        if self.droprate > 0:
+            out = F.dropout(self.conv1(self.relu1(self.bn1(self.layer(x)))), p=self.droprate, inplace=False, training=self.training)
+        return F.avg_pool2d(out, 2)
+    
+
+
+    
+class DDCBNet(nn.Module):
+    """DDCB Net for object detection
+    The network is based on the SSD architecture.
+    Each multibox layer branches into
+        1) conv2d for class conf scores
+        2) conv2d for localization predictions
+        3) associated priorbox layer to produce default bounding
+           boxes specific to the layer's feature map size.
+    See: https://arxiv.org/pdf/1711.07767.pdf for more details on RFB Net.
+
+    Args:
+        phase: (string) Can be "test" or "train"
+        base: VGG16 layers for input, size of either 300 or 512
+        extras: extra layers that feed to multibox loc and conf layers
+        head: "multibox head" consists of loc and conf conv layers
+    """
+
+    def __init__(self, phase, size, base, extras, head, num_classes):
+        super(DDCBNet, self).__init__()
+        self.phase = phase
+        self.num_classes = num_classes
+        self.size = size
+        block = BottleneckBlock
+        if size == 300:
+            self.indicator = 3
+        elif size == 512:
+            self.indicator = 5
+        else:
+            print("Error: Sorry only SSD300 and SSD512 are supported!")
+            return
+        # vgg network
+        self.base = nn.ModuleList(base)
+        # conv_4
+        self.Norm = DenseDDCB(3, 512, 512, 12, block, dropRate=0.0)
+        self.extras = nn.ModuleList(extras)
+        self.loc = nn.ModuleList(head[0])
+        self.conf = nn.ModuleList(head[1])
+        if self.phase == 'test':
+            self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """Applies network layers and ops on input image(s) x.
+
+        Args:
+            x: input image or batch of images. Shape: [batch,3*batch,300,300].
+
+        Return:
+            Depending on phase:
+            test:
+                list of concat outputs from:
+                    1: softmax layers, Shape: [batch*num_priors,num_classes]
+                    2: localization layers, Shape: [batch,num_priors*4]
+                    3: priorbox layers, Shape: [2,num_priors*4]
+
+            train:
+                list of concat outputs from:
+                    1: confidence layers, Shape: [batch*num_priors,num_classes]
+                    2: localization layers, Shape: [batch,num_priors*4]
+                    3: priorbox layers, Shape: [2,num_priors*4]
+        """
+        sources = list()
+        loc = list()
+        conf = list()
+
+        # apply vgg up to conv4_3 relu
+        for k in range(23):
+            x = self.base[k](x)
+
+        s = self.Norm(x)
+        sources.append(s)
+
+        # apply vgg up to fc7
+        for k in range(23, len(self.base)):
+            x = self.base[k](x)
+
+        # apply extra layers and cache source layer outputs
+        for k, v in enumerate(self.extras):
+            x = v(x)
+            if k < self.indicator or k%2 ==0:
+                sources.append(x)
+
+        # apply multibox head to source layers
+        for (x, l, c) in zip(sources, self.loc, self.conf):
+            loc.append(l(x).permute(0, 2, 3, 1).contiguous())
+            conf.append(c(x).permute(0, 2, 3, 1).contiguous())
+
+        #print([o.size() for o in loc])
+
+
+        loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
+        conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
+
+        if self.phase == "test":
+            output = (
+                loc.view(loc.size(0), -1, 4),                   # loc preds
+                self.softmax(conf.view(-1, self.num_classes)),  # conf preds
+            )
+        else:
+            output = (
+                loc.view(loc.size(0), -1, 4),
+                conf.view(conf.size(0), -1, self.num_classes),
+            )
+        return output
+
+    def load_weights(self, base_file):
+        other, ext = os.path.splitext(base_file)
+        if ext == '.pkl' or '.pth':
+            print('Loading weights into state dict...')
+            self.load_state_dict(torch.load(base_file))
+            print('Finished!')
+        else:
+            print('Sorry only .pth and .pkl files supported.')
+            
+def vgg(cfg, i, batch_norm=False):
+    layers = []
+    in_channels = i
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif v == 'C':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+    conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6)
+    conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
+    layers += [pool5, conv6,
+               nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=True)]
+    return layers
+
+base = {
+    '300': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
+            512, 512, 512],
+    '512': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
+            512, 512, 512],
+}
+    
+def add_extras(size, cfg, i, nb_layers, grow_rate, block, dropRate, batch_norm=False):
+    # Extra layers added to VGG for feature scaling
+    layers = []
+    in_channels = i
+    flag = False
+    if size == 512:
+        layers += [BasicConv(256,128,kernel_size=1,stride=1)]
+        layers += [BasicConv(128,256,kernel_size=4,stride=1,padding=1)]
+    elif size ==300:
+        layers += [DenseDDCB(nb_laters, in_channels, cfg[0],grow_rate, block, dropRate)]
+        layers += [DenseDDCB_a(nb_laters, cfg[0], cfg[0],grow_rate, block, dropRate)]
+        layers += [DenseDDCB_a(nb_laters, cfg[1], cfg[1],grow_rate, block, dropRate)]
+        layers += [BasicConv(cfg[2],128,kernel_size=1,stride=1)]
+        layers += [BasicConv(128,cfg[2],kernel_size=3,stride=1)]
+        layers += [BasicConv(256,128,kernel_size=1,stride=1)]
+        layers += [BasicConv(128,256,kernel_size=3,stride=1)]
+    else:
+        print("Error: Sorry only RFBNet300 and RFBNet512 are supported!")
+        return
+    return layers
+
+extras = {
+    '300': [1024, 512, 256],
+    '512': [1024, 'S', 512, 'S', 256, 'S', 256,'S',256],
+}
+
+    
+def multibox(size, vgg, extra_layers, cfg, num_classes):
+    loc_layers = []
+    conf_layers = []
+    vgg_source = [-2]
+    for k, v in enumerate(vgg_source):
+        if k == 0:
+            loc_layers += [nn.Conv2d(512,
+                                 cfg[k] * 4, kernel_size=3, padding=1)]
+            conf_layers +=[nn.Conv2d(512,
+                                 cfg[k] * num_classes, kernel_size=3, padding=1)]
+        else:
+            loc_layers += [nn.Conv2d(vgg[v].out_channels,
+                                 cfg[k] * 4, kernel_size=3, padding=1)]
+            conf_layers += [nn.Conv2d(vgg[v].out_channels,
+                        cfg[k] * num_classes, kernel_size=3, padding=1)]
+    i = 1
+    indicator = 0
+    if size == 300:
+        indicator = 3
+    elif size == 512:
+        indicator = 5
+    else:
+        print("Error: Sorry only RFBNet300 and RFBNet512 are supported!")
+        return
+
+    for k, v in enumerate(extra_layers):
+        if k < indicator or k%2== 0:
+            loc_layers += [nn.Conv2d(v.out_channels, cfg[i]
+                                 * 4, kernel_size=3, padding=1)]
+            conf_layers += [nn.Conv2d(v.out_channels, cfg[i]
+                                  * num_classes, kernel_size=3, padding=1)]
+            i +=1
+    return vgg, extra_layers, (loc_layers, conf_layers)
+
+mbox = {
+    '300': [6, 6, 6, 6, 4, 4],  # number of boxes per feature map location
+    '512': [6, 6, 6, 6, 6, 4, 4],
+}
+
+
+def build_net(phase, size=300, num_classes=21):
+    if phase != "test" and phase != "train":
+        print("Error: Phase not recognized")
+        return
+    if size != 300 and size != 512:
+        print("Error: Sorry only RFBNet300 and RFBNet512 are supported!")
+        return
+
+    return RFBNet(phase, size, *multibox(size, vgg(base[str(size)], 3),
+                                add_extras(size, extras[str(size)], 1024, 3, 12, BottleneckBlock),
+                                mbox[str(size)], num_classes), num_classes)  
